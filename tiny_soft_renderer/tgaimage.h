d729cd46d432a34b184cc3094bf76adddcf653c9
guanyq <guanyq@mapbar.com>
2017-04-27 17:17:21 +0800

    fix warning

--- tiny_soft_renderer/tgaimage.h
+++ tiny_soft_renderer/tgaimage.h
@@ -24,24 +24,24 @@ struct TGAColor {
     unsigned char bgra[4];
     unsigned char bytespp;
 
-    TGAColor() : bgra(), bytespp(1) {
-        for (int i=0; i<4; i++) bgra[i] = 0;
-    }
+	TGAColor() : bytespp(1) {
+		for (int i = 0; i < 4; i++) bgra[i] = 0;
+	}
 
-    TGAColor(unsigned char R, unsigned char G, unsigned char B, unsigned char A=255) : bgra(), bytespp(4) {
+    TGAColor(unsigned char R, unsigned char G, unsigned char B, unsigned char A=255) : bytespp(4) {
         bgra[0] = B;
         bgra[1] = G;
         bgra[2] = R;
         bgra[3] = A;
     }
 
-    TGAColor(unsigned char v) : bgra(), bytespp(1) {
+    TGAColor(unsigned char v) : bytespp(1) {
         for (int i=0; i<4; i++) bgra[i] = 0;
         bgra[0] = v;
     }
 
 
-    TGAColor(const unsigned char *p, unsigned char bpp) : bgra(), bytespp(bpp) {
+    TGAColor(const unsigned char *p, unsigned char bpp) : bytespp(bpp) {
         for (int i=0; i<(int)bpp; i++) {
             bgra[i] = p[i];
         }
@@ -55,7 +55,7 @@ struct TGAColor {
     TGAColor operator *(float intensity) const {
         TGAColor res = *this;
         intensity = (intensity>1.f?1.f:(intensity<0.f?0.f:intensity));
-        for (int i=0; i<4; i++) res.bgra[i] = bgra[i]*intensity;
+		for (int i = 0; i<4; i++) res.bgra[i] = unsigned char(bgra[i] * intensity);
         return res;
     }
 };
