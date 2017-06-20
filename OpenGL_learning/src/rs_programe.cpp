1ebb2ea27e77902a0d2c07247f69a3a03f923dd1
guanyq <guanyq@mapbar.com>
2017-06-20 11:05:08 +0800

    refactor

--- OpenGL_learning/src/rs_programe.cpp
+++ OpenGL_learning/src/rs_programe.cpp
@@ -53,3 +53,88 @@ RsProgram::~RsProgram()
 	cout<<"we are now delete the shader program"<<endl;
 	glDeleteProgram(m_program);
 }
+
+GLuint RsProgram::getShaderProgram()
+{ 
+	return m_program; 
+}
+
+void RsProgram::usePrograme() 
+{
+	glUseProgram(m_program);
+}
+
+GLuint RsProgram::getUniformLoc(GLchar* name) 
+{
+	return glGetUniformLocation(m_program, name);
+}
+
+GLuint RsProgram::getAttributeLoc(GLchar* name) 
+{
+	return glGetAttribLocation(m_program, name);
+}
+
+void RsProgram::setUniform1f(GLuint loc, float value) 
+{
+	glUniform1f(loc, value);
+}
+
+void RsProgram::setUniform2fv(GLuint loc, vec2f vec) {
+	glUniform2f(loc, vec.x, vec.y);
+}
+
+void RsProgram::setUniform2f(GLuint loc, float v0, float v1) 
+{
+	glUniform2f(loc, v0, v1);
+}
+
+void RsProgram::setUniform3fv(GLuint loc, float* vec) 
+{
+	glUniform3fv(loc, 3, vec);
+}
+
+void RsProgram::setUniform3fv(GLuint loc, vec3f vec) 
+{
+	setUniform3f(loc, vec.x, vec.y, vec.z);
+}
+
+void RsProgram::setUniform3f(GLuint loc, float v0, float v1, float v2)
+{
+	glUniform3f(loc, v0, v1, v2);
+}
+
+void RsProgram::setUniform4fv(GLuint loc, float* vec)
+{
+	glUniform4fv(loc, 4, vec);
+}
+
+void RsProgram::setUniform4fv(GLuint loc, vec4f vec)
+{
+	setUniform4f(loc, vec.x, vec.y, vec.z, vec.w);
+}
+
+void RsProgram::setUniform4f(GLuint loc, float v1, float v2, float v3, float v4)
+{
+	glUniform4f(loc, v1, v2, v3, v4);
+}
+
+void RsProgram::setAttribute1f(GLuint loc, float value)
+{
+	glVertexAttrib1f(loc, value);
+}
+
+void RsProgram::setAttribute3fv(GLuint loc, float* value)
+{
+	glVertexAttrib3fv(loc, value);
+}
+
+void RsProgram::setAttribute3f(GLuint loc, float x, float y, float z) 
+{
+	glVertexAttrib3f(loc, x, y, z);
+}
+
+void RsProgram::setVertexBuffer(GLuint loc, int size, GLvoid* val)
+{
+	glVertexAttribPointer(loc, size, GL_FLOAT, GL_FALSE, size*sizeof(float), val);
+	glEnableVertexAttribArray(loc);
+}
--- OpenGL_learning/src/rs_programe.h
+++ OpenGL_learning/src/rs_programe.h
@@ -5,78 +5,43 @@ class RsProgram
 {
 public:
 	RsProgram(const GLchar* vertexShaderSource, const GLchar* fragmentShaderSource);
+
 	~RsProgram();
 
-	GLuint getShaderProgram() { return m_program; }
+	GLuint getShaderProgram();
 
-	void usePrograme() {
-		glUseProgram(m_program);
-	}
+	void usePrograme();
 
-	GLuint getUniformLoc(GLchar* name) {
-		return glGetUniformLocation(m_program, name);
-	}
+	GLuint getUniformLoc(GLchar* name);
 
-	GLuint getAttributeLoc(GLchar* name) {
-		return glGetAttribLocation(m_program, name);
-	}
+	GLuint getAttributeLoc(GLchar* name);
 
-	void setUniform1f(GLuint loc, float value) {
-		glUniform1f(loc, value);
-	}
+	void setUniform1f(GLuint loc, float value);
 	
-	void setUniform2fv(GLuint loc, vec2f vec) {
-		glUniform2f(loc, vec.x, vec.y);
-	}
-	void setUniform3fv(GLuint loc, float* vec) {
-		glUniform3fv(loc, 3, vec);
-	}
+	void setUniform2fv(GLuint loc, vec2f vec);
 
-	void setUniform3fv(GLuint loc, vec3f vec) {
-		glUniform3f(loc, vec.x, vec.y, vec.z);
-	}
+	void setUniform2f(GLuint loc, float v0, float v1);
 
-	void setUniform3f(GLuint loc, float value1, float value2, float value3) {
-		glUniform3f(loc, value1, value2, value3);
-	}
+	void setUniform3fv(GLuint loc, float* vec);
 
-	void setUniform4fv(GLuint loc, float* vec) {
-		float temp;
-		for (int i = 0; i < 4; i++)
-		{
-			temp = vec[i];
-		}
-		glUniform4fv(loc, 4, vec);
-		glUniform4f(loc, vec[0], vec[1], vec[2], vec[3]);
-	}
+	void setUniform3fv(GLuint loc, vec3f vec);
 
-	void setUniform4fv(GLuint loc, vec4f vec) {
-		glUniform4fv(loc, 4, (float*)&vec.x);
-	}
+	void setUniform3f(GLuint loc, float v0, float v1, float v2);
 
-	void setUniform4f(GLuint loc, float value1, float value2, float value3, float value4) {
-		glUniform4f(loc, value1, value2, value3, value4);
-	}
+	void setUniform4fv(GLuint loc, float* vec);
 
+	void setUniform4fv(GLuint loc, vec4f vec);
 
-	void setAttribute1f(GLuint loc, float value) {
-		glVertexAttrib1f(loc, value);
-	}
+	void setUniform4f(GLuint loc, float v1, float v2, float v3, float v4);
 
-	void setAttribute3fv(GLuint loc, float* value) {
-		glVertexAttrib3fv(loc, value);
-	}
+	void setAttribute1f(GLuint loc, float value);
+	
+	void setAttribute3fv(GLuint loc, float* value);
 
-	void setAttribute3f(GLuint loc, float x, float y, float z) {
-		glVertexAttrib3f(loc, x, y, z);
-	}
+	void setAttribute3f(GLuint loc, float x, float y, float z);
 
-	void setVertexBuffer(GLuint loc, int size, GLvoid* val)
-	{
-		glVertexAttribPointer(loc,size,GL_FLOAT,GL_FALSE,size*sizeof(float),val);
-		glEnableVertexAttribArray(loc);
-	}
-
+	void setVertexBuffer(GLuint loc, int size, GLvoid* val);
+	
 private:
 	GLuint createProgram(const GLchar*_vertexShaderSource, const GLchar*_fragmentShaderSource);
 
